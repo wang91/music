fb629ba181fb9caff84056690e99fc1ba67ee21f
Youke <1078865902@qq.com>
2023-02-08 16:29:30 +0800

    A 新增主界面，录制界面

---
+++ MusicStudio/Controller/RootController.swift
@@ -0,0 +1,25 @@
+//
+//  RootController.swift
+//  MusicStudio
+//
+//  Created by YouKe Wang on 2023/2/8.
+//
+
+import UIKit
+
+class RootController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func recordAudio(_ sender: UIButton) {
+        self.navigationController?.pushViewController(RecordController(nibName: "RecordController", bundle: nil), animated: true)
+    }
+    
+    @IBAction func playMusic(_ sender: UIButton) {
+        self.navigationController?.pushViewController(MusicController(nibName: "MusicController", bundle: nil), animated: true)
+    }
+}
--- MusicStudio/SceneDelegate.swift
+++ MusicStudio/SceneDelegate.swift
@@ -16,7 +16,18 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        
+        
         guard let _ = (scene as? UIWindowScene) else { return }
+        
+        if let windowSence = scene as? UIWindowScene  {
+            self.window = UIWindow(windowScene: windowSence)
+            self.window?.backgroundColor = .white
+            let navVC = UINavigationController(rootViewController: RootController(nibName: "RootContorller", bundle: nil))
+            navVC.view.backgroundColor = .white
+            self.window?.rootViewController = navVC
+            self.window?.makeKeyAndVisible()
+        }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
